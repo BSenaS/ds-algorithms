a95beacd0caeb8dfc112488afafc5a4967b2c5cb
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-07-15 16:59:33 +0300

    LinkedList

--- algorithms/index.js
+++ algorithms/index.js
@@ -325,3 +325,304 @@
 //     }
 //   }
 // }
+
+//LINKED LIST
+//WITHOUT TAIL VERSION
+//1.NODE TANIMLAMASI
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+
+// //2.LinkedList tanımlaması
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.size = 0;
+//   }
+
+//   isEmpty() {
+//     return this.size === 0;
+//   }
+
+//   getSize() {
+//     return this.size;
+//   }
+
+//   //Listin başına node ekleme.
+//   prepend(value) {
+//     const node = new Node(value);
+//     //Eğer list null ise, eklenen değer ilk değer ise o değeri (Head) yaparız.
+//     if (this.isEmpty()) {
+//       this.head = node;
+//     } else {
+//       node.next = this.head;
+//       this.head = node;
+//     }
+//     this.size++;
+//   }
+
+//   //List'in sonuna değer ekleme.(Listteki son eleman prev oluyor)
+//   //Big-O -> O(1) : Constant
+//   append(value) {
+//     const node = new Node(value);
+
+//     if (this.isEmpty()) {
+//       this.head = node;
+//     } else {
+//       let prev = this.head;
+//       while (prev.next) {
+//         prev = prev.next;
+//       }
+//       prev.next = node;
+//     }
+//     this.size++;
+//   }
+
+//   //Belirli bir indexe veri kaydetme.
+//   insert(value, index) {
+//     if (index < 0 || index > this.size) {
+//       return;
+//     }
+//     if (index === 0) {
+//       this.prepend(value);
+//     } else {
+//       const node = new Node(value);
+//       let prev = this.head;
+//       for (let i = 0; i < index - 1; i++) {
+//         prev = prev.next;
+//       }
+//       node.next = prev.next;
+//       prev.next = node;
+//       this.size++;
+//     }
+//   }
+
+//   //Verilen indexteki değeri silme.
+//   removeFrom(index) {
+//     if (index < 0 || index >= this.size) {
+//       return null;
+//     }
+//     let removeNode;
+//     if (index === 0) {
+//       removeNode = this.head;
+//       this.head = this.head.next;
+//     } else {
+//       let prev = this.head;
+//       for (let i = 0; i < index - 1; i++) {
+//         prev = prev.next;
+//       }
+//       removeNode = prev.next;
+//       prev.next = removeNode.next;
+//     }
+//     this.size--;
+//     return removeNode.value;
+//   }
+
+//   //Verilen valueyi listten çıkarma.
+//   //Big-o (On)
+//   removeValue(value) {
+//     if (this.isEmpty()) {
+//       return null;
+//     }
+//     let removedNode;
+
+//     if (value === this.head.value) {
+//       this.head = this.head.next;
+//       this.size--;
+//       return value;
+//     } else {
+//       let prev = this.head;
+//       while (prev.next && prev.next.value !== value) {
+//         prev = prev.next;
+//       }
+//       if (prev.next) {
+//         removedNode = prev.next;
+//         prev.next = removedNode.next;
+//         this.size--;
+//         return value;
+//       }
+//       return null;
+//     }
+//   }
+
+//   //Verilen Value'nin indexini bulma, yoksa -1 dön.
+//   search(value) {
+//     if (this.isEmpty()) {
+//       return -1;
+//     }
+//     let i = 0; //index
+//     let curr = this.head;
+//     while (curr) {
+//       if (curr.value === value) {
+//         return i;
+//       }
+//       curr = curr.next;
+//       i++;
+//     }
+//     return -1;
+//   }
+
+//   //Linked List Reverse
+
+//   reverse() {
+//     let prev = null;
+//     let curr = this.head;
+//     while (curr) {
+//       let next = curr.next;
+//       curr.next = prev;
+//       prev = curr;
+//       curr = next;
+//     }
+//     this.head = prev;
+//   }
+
+//   //Big-O -> O(n) : Lineer
+//   print() {
+//     if (this.isEmpty()) {
+//       console.log("List is empty");
+//     } else {
+//       let curr = this.head;
+//       let listValues = "";
+//       while (curr) {
+//         listValues += `${curr.value} `;
+//         curr = curr.next;
+//       }
+//       console.log(listValues);
+//     }
+//   }
+// }
+
+// const list = new LinkedList();
+// console.log("List is empty? -> ", list.isEmpty());
+// console.log("List size: ", list.getSize());
+
+// list.print();
+
+// list.insert(10, 0);
+// list.insert(20, 1);
+// list.insert(30, 2);
+// list.insert(40, 3);
+
+// list.print();
+
+// list.reverse();
+// list.print();
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+//2.LinkedList tanımlaması [with TAIL version]
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  //Big-O -> O(n) : Lineer
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.size++;
+  }
+
+  removeFromFront() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      const value = this.head.value;
+      this.head = this.head.next;
+      this.size--;
+      return value;
+    }
+  }
+
+  removeFromEnd() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const value = this.tail.value;
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let prev = this.head;
+      while (prev.next !== this.tail) {
+        prev = prev.next;
+      }
+      prev.next = null;
+      this.tail = prev;
+    }
+    this.size--;
+    return value;
+  }
+}
+
+const list = new LinkedList();
+
+console.log("List is empty?", list.isEmpty());
+console.log("List size?", list.getSize());
+list.print();
+
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+
+list.print();
+
+list.removeFromEnd();
+list.print();
+
+list.removeFromFront();
+
+list.print();
